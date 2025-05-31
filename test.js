fb6e58f9f3821b3bbbf2f438ec98ccba3f1adfd6
Alkem0s <efe2004alkim@gmail.com>
2025-05-31 18:18:50 +0300

    cleanup

--- test.js
+++
@@ -1,9 +0,0 @@
-import { initializeFirebase } from './firebase.js';
-import { getAllPassengers, setPassenger, assignVehicleToRoute } from './api.js';
-
-// Initialize Firebase first
-await initializeFirebase();
-
-// Now you can use the APIs
-const passengers = await getAllPassengers();
-console.log(passengers);
